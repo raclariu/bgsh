fb3c7453d0bfd0abd883ebc6f91dc09986532efb
CLAUDIU RACLARIU <ssrakla@gmail.com>
2022-04-16 01:34:19 +0300

    tweaked settings

--- server/config/ecosystem.config.cjs
+++ server/config/ecosystem.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
 		{
 			name               : 'server',
 			script             : './server.js',
-			error_file         : '../logs/err.log',
-			out_file           : '../logs/out.log',
+			error_file         : './logs/err.log',
+			out_file           : './logs/out.log',
 			log_date_format    : 'DD-MM-YYYY HH:mm:ss',
 			watch              : true,
 			max_memory_restart : '200M'
